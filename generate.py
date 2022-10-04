5dc79ec438d4e8f52000d3ae3e02a1dc469ef6df
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2022-10-03 21:43:02 -0700

    Have all external links open in new tabs.
    Have generate.py find all external links and change them to open in a
    new tab when clicked.

--- generate.py
+++ generate.py
@@ -7,6 +7,12 @@ CONTENT_DIR = 'html'
 SERVED_DIR = 'e'
 
 def main():
+	'''Fill in templates with page content and other parameters.
+	I did try (22-09) to use Python's real XML parsing here, but I found it did not work well at all because HTML is not really a subset of XML. In particular:
+	* Attributes need to have values (and allowfullscreen in YouTube embeds does not).
+	* All ampersands need to be escaped, even in quoted attribute values.
+	* Other escapes like &nbsp; can*not* be used.'''
+
 	for file_ in os.listdir(CONTENT_DIR):
 		if file_.endswith('.xml'):
 			print(f'processing {file_}')
@@ -14,6 +20,8 @@ def main():
 			if 'template' not in pageData:
 				print(f'Error: `{file_}` does not specify a template to use.', file=sys.stderr)
 				continue
+			pageData['main'] = re.sub(r" href='https?://.*?'", r"\g<0> target='_blank'", pageData['main'])
+
 			with open(f'{TEMPLATE_DIR}/{pageData["template"]}.html') as templateFile:
 				with open(f'{SERVED_DIR}/{file_[:-4]}.html', 'w') as constructedFile:
 					try:
